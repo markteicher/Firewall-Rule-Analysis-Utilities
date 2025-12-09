a2ec5e846f25d34157d01efa8c1cd72747d342ef
Mark Teicher <mark.teicher@gmail.com>
2025-12-08 22:08:05 -0500

    Update firemon_input.py

--- bin/firemon_input.py
+++ bin/firemon_input.py
@@ -1,1 +1,40 @@
+#!/usr/bin/env python
+# encoding = utf-8
 
+import sys
+import json
+import time
+import datetime
+import logging
+import requests
+from splunklib.modularinput import *
+
+class FireMonInput(Script):
+def get_scheme(self):
+scheme = Scheme("FireMon Data Collector")
+scheme.description = "Collects data from FireMon API."
+scheme.use_external_validation = True
+scheme.use_single_instance = False
+
+scheme.add_argument(
+Argument("firemon_url",
+title="FireMon API Base URL",
+description="Base URL of the FireMon Security Manager API",
+required_on_create=True)
+)
+
+scheme.add_argument(
+Argument("api_key",
+title="FireMon API Key",
+description="Your FireMon API key",
+required_on_create=True)
+)
+
+scheme.add_argument(
+Argument("interval",
+title="Polling interval (in seconds)",
+description="How often to poll the FireMon API",
+required_on_create=True)
+)
+
+return scheme
